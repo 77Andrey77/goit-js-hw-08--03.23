3875e5906bf6319bf23d7c1ae60104f0085821ee
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2023-03-07 14:05:09 +0200

    fin 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) =>
+`
+<a class="gallery__item" href="${original}" title="${description}" >
+  <img class="gallery__image" src="${preview}" alt="${description}" captionPosition />
+</a>
+`
+).join('');
+
+const galleryEl = document.querySelector('.gallery');
+//рендер разметки
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+ new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay: 1000,captionPosition: "bottom",});
